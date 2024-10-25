b59bea4819ac6a7bbcb310d5ad0917857ba41993
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-10-25 17:24:13 -0300

    chore: config jest-styled-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,12 +3,16 @@ import { render, screen } from '@testing-library/react';
 import Main from '.';
 
 describe('<Main />', () => {
-  it('should render the heading', () => {
+  it('should render the heading with the right text and backgroudn color', () => {
     const { container } = render(<Main />);
 
     expect(
       screen.getByRole('heading', { name: /hello, world!/i }),
     ).toBeInTheDocument();
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#ffffff',
+    });
 
     expect(container.firstChild).toMatchSnapshot();
   });
